177f8c4a270ab0ab92e8396273b6cd699089eab7
Léo <89333343+LeoCirp@users.noreply.github.com>
2023-04-03 07:30:55 -0500

    Add files via upload

---
+++ gs_restaure/lua/autorun/config.lua
@@ -0,0 +1,23 @@
+GS = {}
+GS["Restauration"] = {}
+Configuration = GS["Restauration"]
+Configuration.ShowDetail = true
+Configuration.Version = 3
+Configuration.Debug = true 
+Configuration.Path = "GS/Restauration Emplacement"
+Configuration.ValeurEntity = {
+    ["edit_fog"] = 100,
+    ["sent_ball"] = 1000
+}
+Configuration.Sound = "145"
+
+
+
+GS.ShowDetail = true
+GS.Version = 3
+GS.Debug = true
+GS.Path = "GS/Restauration Emplacement"
+GS.ValeurEntity = {
+    ["edit_fog"] = 100,
+    ["sent_ball"] = 1000
+}
