446601bcc1a609ce9dae279ce0408cd99664554e
Finnwinch <leocirpaci@outlook.com>
2023-10-06 11:16:54 -0400

    change print format to individuel file

--- gs_restaure/lua/autorun/main.lua
+++ gs_restaure/lua/autorun/main.lua
@@ -1,15 +1,6 @@
 if not GS then GS = {} end
-MsgC(
-    Color(153, 0, 255), "[", 
-    Color(255,0,0), "GS", 
-    Color(153, 0, 255), "]", 
-    Color(0,0,255), " Resauration System ", 
-    Color(0,255,0), GS.Version,
-    Color(0,0,0),"\n"
-)
-if GS.ShowDetail then
-    MsgC( Color( 255, 255, 255, 200), "System en cas de dectection de crash / time out, permet de replacer les jouers sur la map\n")
-    MsgC( Color( 255, 0, 0, 200), "Par Finnwinch\n")
+if SERVER then
+    include("gs_restaure/affichage_script_run.lua")
 end
 local PATH = "GS/Restauration Emplacement"
 PATH = GS.Path || PATH
